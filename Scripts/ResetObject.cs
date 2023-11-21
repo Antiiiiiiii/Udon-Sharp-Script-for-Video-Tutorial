2aceaa8f892b2aa7e4842e00118633e9633c8888
Antiiiiiiii <121515190+Antiiiiiiii@users.noreply.github.com>
2023-11-22 01:42:29 +0900

    Add files via upload

---
+++ Scripts/ResetObject.cs
@@ -0,0 +1,35 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+
+public class ResetObject : UdonSharpBehaviour
+{
+    [SerializeField] private GameObject[] controlledObjects;
+    private Vector3[] initialPositions;
+    private Quaternion[] initialRotations;
+
+    void Start()
+    {
+        initialPositions = new Vector3[controlledObjects.Length];
+        initialRotations = new Quaternion[controlledObjects.Length];
+        for (int i = 0; i < controlledObjects.Length; i++)
+        {
+            initialPositions[i] = controlledObjects[i].transform.position;
+            initialRotations[i] = controlledObjects[i].transform.rotation;
+        }
+    }
+
+    public void Reset()
+    {
+        for (int i = 0; i < controlledObjects.Length; i++)
+        {
+            Networking.SetOwner(Networking.LocalPlayer, controlledObjects[i]);
+            controlledObjects[i].transform.position = initialPositions[i];
+            controlledObjects[i].transform.rotation = initialRotations[i];
+        }
+    }
+
+}
---
+++ Scripts/SimpleResetObject.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+
+public class SimpleResetObject : UdonSharpBehaviour
+{
+    [SerializeField] private GameObject target;
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+
+    void Start()
+    {
+        initialPosition = target.transform.position;
+        initialRotation = target.transform.rotation;
+    }
+
+    public void Reset()
+    {
+        Networking.SetOwner(Networking.LocalPlayer, target);
+        target.transform.position = initialPosition;
+        target.transform.rotation = initialRotation;
+    }
+}
