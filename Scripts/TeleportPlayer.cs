f62a491df73127573a83bab9c28485af36335e6d
Antiiiiiiii <121515190+Antiiiiiiii@users.noreply.github.com>
2023-11-22 01:23:39 +0900

    Add files via upload

---
+++ Scripts/TeleportPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TeleportPlayer : UdonSharpBehaviour
+{
+    [SerializeField] Transform targetPosition;
+
+    public void OnPlayerTriggerEnter()
+    {
+        Networking.LocalPlayer.TeleportTo(targetPosition.position, targetPosition.rotation);
+    }
+}
